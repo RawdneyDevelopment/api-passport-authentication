455f88bcc1f5d692d3ab4e7a89f13d3062f23c25
Rawdney Mendes <mendesrawdney@gmail.com>
2022-11-07 14:01:15 -0300

    added: new router register, login to aplication

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 /* Routes*/
 app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
